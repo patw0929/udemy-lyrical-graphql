e88a1ad0bfe5d0374fef98e43bf8de1f71ec1c75
patw <patw.hi@gmail.com>
2018-12-19 01:27:07 +0800

    Create song form

---
+++ client/components/SongCreate.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      id
+      title
+    }
+  }
+`;
+
+class SongCreate extends Component {
+  state = {
+    title: '',
+  };
+
+  handleChange = e => {
+    e.persist();
+    this.setState(prevState => ({
+      title: e.target.value,
+    }));
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.mutate({
+      variables: {
+        title: this.state.title,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>Create a New Song</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label>Song Title:</label>
+          <input
+            type="text"
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default graphql(mutation)(SongCreate);
