788a2e5222a6601d8b2bb8915c2bd096739daf69
patw <patw.hi@gmail.com>
2018-12-19 10:51:11 +0800

    SongDetail page & load data from apollo

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+
+import query from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
+    const { loading, song } = this.props.data;
+
+    if (loading || !song) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        <h3>Song Detail</h3>
+        <Link to="/">Back</Link>
+
+        <h3>{song.title}</h3>
       </div>
     );
   }
 }
 
-export default SongDetail;
+export default graphql(query, {
+  options: props => {
+    return {
+      variables: {
+        id: props.params.id,
+      },
+    };
+  },
+})(SongDetail);
